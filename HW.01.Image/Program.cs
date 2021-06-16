0e8a62c312abba502e46b04a3bdec98418d979e6
ulia <juliabuiko@gmail.com>
2021-06-15 21:34:37 -0400

    test

---
+++ HW.01.Image/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW._01.Image
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
