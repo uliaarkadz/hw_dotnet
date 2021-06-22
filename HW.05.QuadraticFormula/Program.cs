51aeea890b8083aba23d8018f269a2b269e3accf
ulia <juliabuiko@gmail.com>
2021-06-21 22:31:21 -0400

    calculator

--- HW.05.QuadraticFormula/Program.cs
+++ HW.05.QuadraticFormula/Program.cs
@@ -2,46 +2,51 @@
 
 namespace HW._05.QuadraticFormula
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            int a,b,c;
-            double d, x1,x2;
+            double a = 0,b = 0,c = 0;
+            double d, x1, x2;
             
-            Console.Write("\n\n");
-            Console.Write("Calculate root of Quadratic Equation :\n");
-            Console.Write("----------------------------------------");
-            Console.Write("\n\n");
- 
+            Console.Write(value: "Calculate root of Quadratic Equation :\n");
+            Console.Write(value: "----------------------------------------\n");
+
             Console.Write("Input the value of a : ");
-            a = Convert.ToInt32(Console.ReadLine());
+            InputNumbers(a);
             Console.Write("Input the value of b : ");
-            b = Convert.ToInt32(Console.ReadLine());
+            InputNumbers(b);
             Console.Write("Input the value of c : ");
-            c = Convert.ToInt32(Console.ReadLine());
+            InputNumbers(c);
 
-            d=b*b-4*a*c;
-            if(d==0)
-            {
-                Console.Write("Both roots are equal.\n");
-                x1=-b/(2.0*a);
-                x2=x1;
-                Console.Write("First  Root Root1= {0}\n",x1);
-                Console.Write("Second Root Root2= {0}\n",x2);
+            // Calculating a discriminant
+            d = b * b - 4 * a * c;
+
+            if (d == 0) {
+                x1 = -b / (2 * a);
+                Console.WriteLine(format: "The only solution is x={0}.", arg0: x1);
+                Console.ReadLine();
             }
-            else if(d>0)
-            {
-                Console.Write("Both roots are real and diff-2\n");
 
-                x1=(-b+Math.Sqrt(d))/(2*a);
-                x2=(-b-Math.Sqrt(d))/(2*a);
-
-                Console.Write("First  Root Root1= {0}\n",x1);
-                Console.Write("Second Root root2= {0}\n",x2);
+            // If d < 0, no real solutions exist
+            else if (d < 0) { 
+                Console.WriteLine(value: "There are no real solutions");
+                Console.ReadLine();
             }
-            else
-                Console.Write("Root are imeainary;\nNo Solution. \n\n");
+
+            // If d > 0, there are two real solutions 
+            else {
+                x1 = (-b - Math.Sqrt(d: d)) / (2 * a);
+                x2 = (-b + Math.Sqrt(d: d)) / (2 * a);
+                Console.WriteLine(format: "x1={0} and x2={1}.", arg0: x1, arg1: x2);
+                Console.ReadLine();
+            }
+        }
+
+        private static double InputNumbers(double number)
+        {
+           number = Convert.ToDouble(Console.ReadLine());
+           return InputNumbers(number);
         }
     }
 }
