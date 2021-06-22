f09cdf2701de7889eec5b2c5448fbcc78ba89552
ulia <juliabuiko@gmail.com>
2021-06-21 22:55:42 -0400

    updates

--- HW.05.QuadraticFormula/Program.cs
+++ HW.05.QuadraticFormula/Program.cs
@@ -6,24 +6,24 @@ namespace HW._05.QuadraticFormula
     {
         static void Main(string[] args)
         {
-            double a = 0,b = 0,c = 0;
+            int a ,b,c;
             double d, x1, x2;
             
             Console.Write(value: "Calculate root of Quadratic Equation :\n");
             Console.Write(value: "----------------------------------------\n");
 
             Console.Write("Input the value of a : ");
-            InputNumbers(a);
+            a = Int32.Parse(Console.ReadLine());
             Console.Write("Input the value of b : ");
-            InputNumbers(b);
+            b = Int32.Parse(Console.ReadLine());
             Console.Write("Input the value of c : ");
-            InputNumbers(c);
+            c = Int32.Parse(Console.ReadLine());
 
             // Calculating a discriminant
             d = b * b - 4 * a * c;
 
             if (d == 0) {
-                x1 = -b / (2 * a);
+                x1 = -b / (a * 2);
                 Console.WriteLine(format: "The only solution is x={0}.", arg0: x1);
                 Console.ReadLine();
             }
@@ -41,12 +41,6 @@ namespace HW._05.QuadraticFormula
                 Console.WriteLine(format: "x1={0} and x2={1}.", arg0: x1, arg1: x2);
                 Console.ReadLine();
             }
-        }
-
-        private static double InputNumbers(double number)
-        {
-           number = Convert.ToDouble(Console.ReadLine());
-           return InputNumbers(number);
         }
     }
 }
