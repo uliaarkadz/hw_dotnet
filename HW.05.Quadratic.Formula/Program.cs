949e5bdb2d10c4292784f90ad3388e0d0bab9d81
ulia <juliabuiko@gmail.com>
2021-06-21 21:25:15 -0400

    init commit

---
+++ HW.05.Quadratic.Formula/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW._05.Quadratic.Formula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
