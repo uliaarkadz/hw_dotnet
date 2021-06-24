cd618b91c739a9382400a8b0fb24dc4d4b6f7b6f
ulia <juliabuiko@gmail.com>
2021-06-23 23:33:07 -0400

    HW3 Task2 adding input for the answer and validaton if the answer is correct

--- HW03.Operators/Program.cs
+++ HW03.Operators/Program.cs
@@ -10,24 +10,36 @@ namespace HW03.Operators
             ReadInteger(out string a);
             Console.Write("Enter Second Number: ");
             ReadInteger(out string b);
+            Console.Write("Enter Result of addition of two numbers: ");
+            ReadInteger(out string resultFromInput);
+             int result = AddTwoNumbers(Convert.ToInt32(a),Convert.ToInt32(b));
 
-            int result = AddTwoNumbers(Convert.ToInt32(a),Convert.ToInt32(b));
-            Console.WriteLine("\nThe sum of two numbers is : {0} \n", result );
-        }
-        static void ReadInteger(out string input)
-        {
-            input = Console.ReadLine();
-            int n;
-            while (!Int32.TryParse(input, out n))
+             while (Convert.ToInt32(resultFromInput) != result)
+             {
+                 Console.WriteLine("\nThe answer is wrong try again\n");
+                 ReadInteger(out  resultFromInput); 
+                 
+                if (Convert.ToInt32(resultFromInput) == result)
+                {
+                    Console.WriteLine("\nThe sum of two numbers is : {0} \n", result);
+                }
+             }
+
+            static void ReadInteger(out string input)
             {
-                Console.WriteLine("Input is not a valid number; enter valid number");
                 input = Console.ReadLine();
+                int n;
+                while (!Int32.TryParse(input, out n))
+                {
+                    Console.WriteLine("Input is not a valid number; enter valid number");
+                    input = Console.ReadLine();
+                }
             }
-        }
 
-        public static int AddTwoNumbers(int num1, int num2)
-        {
-            return num1 + num2;
+             static int AddTwoNumbers(int num1, int num2)
+            {
+                return num1 + num2;
+            }
         }
     }
 }
