5c2c350d833637ef87f4add26abe2e21acf71ca0
ulia <juliabuiko@gmail.com>
2021-06-23 23:05:45 -0400

    HW03 Task 1 complete, add two numbers with input validation

---
+++ HW03.Operators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HW03.Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number: ");
+            ReadInteger(out string a);
+            Console.Write("Enter Second Number: ");
+            ReadInteger(out string b);
+
+            int result = AddTwoNumbers(Convert.ToInt32(a),Convert.ToInt32(b));
+            Console.WriteLine("\nThe sum of two numbers is : {0} \n", result );
+        }
+        static void ReadInteger(out string input)
+        {
+            input = Console.ReadLine();
+            int n;
+            while (!Int32.TryParse(input, out n))
+            {
+                Console.WriteLine("Input is not a valid number; enter valid number");
+                input = Console.ReadLine();
+            }
+        }
+
+        public static int AddTwoNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
