e4f387ee8caf9cb6a4d896cd1ca2199e4172472f
ulia <juliabuiko@gmail.com>
2021-06-25 20:26:11 -0400

    adding 3 task validate the result correct or not

--- HW03.Operators/Program.cs
+++ HW03.Operators/Program.cs
@@ -12,34 +12,47 @@ namespace HW03.Operators
             ReadInteger(out string b);
             Console.Write("Enter Result of addition of two numbers: ");
             ReadInteger(out string resultFromInput);
-             int result = AddTwoNumbers(Convert.ToInt32(a),Convert.ToInt32(b));
+            int result = AddTwoNumbers(Convert.ToInt32(a),Convert.ToInt32(b));
+            
+            while (result != Convert.ToInt32(resultFromInput) || result == Convert.ToInt32(resultFromInput))
+            {
 
-             while (Convert.ToInt32(resultFromInput) != result)
-             {
-                 Console.WriteLine("\nThe answer is wrong try again\n");
-                 ReadInteger(out  resultFromInput); 
-                 
                 if (Convert.ToInt32(resultFromInput) == result)
                 {
-                    Console.WriteLine("\nThe sum of two numbers is : {0} \n", result);
+                    Console.WriteLine("\nCorrect! The sum of two numbers is : {0} \n", result);
+                    break;
                 }
-             }
 
-            static void ReadInteger(out string input)
-            {
-                input = Console.ReadLine();
-                int n;
-                while (!Int32.TryParse(input, out n))
+                
+                else if (Convert.ToInt32(resultFromInput) < result)
                 {
-                    Console.WriteLine("Input is not a valid number; enter valid number");
-                    input = Console.ReadLine();
-                }
-            }
+                    Console.WriteLine("\nThe answer should be greater, try again\n");
+                    ReadInteger(out resultFromInput);
 
-             static int AddTwoNumbers(int num1, int num2)
-            {
-                return num1 + num2;
+                }
+                else if (Convert.ToInt32(resultFromInput) > result)
+                {
+                    Console.WriteLine("\nThe answer should be less, try again\n");
+                    ReadInteger(out resultFromInput);
+                }
+                
             }
         }
+
+        static void ReadInteger(out string input)
+        {
+            input = Console.ReadLine();
+            int n;
+            while (!Int32.TryParse(input, out n))
+            {
+                Console.WriteLine("Input is not a valid number; enter valid number");
+                input = Console.ReadLine();
+            }
+        }
+
+        static int AddTwoNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        }
     }
-}
