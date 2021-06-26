84c1ffba5da2f8539b8995dc08db8f9f98680eb6
ulia <juliabuiko@gmail.com>
2021-06-25 23:48:11 -0400

    Print reversed alphabet task

---
+++ Alpabet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Alpabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char alphabet = 'z'; alphabet >= 'a'; alphabet--)
+            {
+                Console.Write(alphabet + " ");
+            }
+            
+            
+            
+        }
+    }
+}
