10b4c2027bb03468a5b0873071f8d7d029c95b05
ulia <juliabuiko@gmail.com>
2021-07-10 00:10:34 -0400

    completing task 2

--- HW.05.ARRAYS.CODEWARS/Program.cs
+++ HW.05.ARRAYS.CODEWARS/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Text;
 
 namespace HW._05.ARRAYS.CODEWARS
 {
@@ -66,55 +66,58 @@ namespace HW._05.ARRAYS.CODEWARS
              заменить все пробелы, расположенные за первым символом '?', на символ подчёркивания '_'.*/
             
             string myString = "1a!2.3!!.. 4.!.?6 7! ?..?";
-            int count = 0;
-            int index = 0;
-            //char question = '?';
-            foreach (char c in myString)
-            {
-                if (c == '?')
-                    count++;
-            }
-
-            char[] newArray = myString.ToCharArray();
-
-            for (int i = 0; i < myString.Length; i++)
-            {
-                if (myString[i] == '!' || myString[i] == '.')
+            
+            StringBuilder sb = new StringBuilder(myString);
+            
+            int index;
+            int pos;
+            
+            for (int i = 0; i < sb.Length; i++)
                 {
-                    myString.Remove(i);
-                    if (myString[i] == '?')
+                    if (sb[i] == '!')
                     {
-                        count++;
-                        
-                    }
-                }
-                    
-            }
-
-            if (count > 1)
-            {
-                for (int i = 0; i < myString.Length; i++)
-                {
-                    if (myString[i] == '?')
                         index = i;
-                            break;
+                        sb.Remove(i,1);
+                    }else if(sb[i] == '?')
+                        break;
+                    }
+            for (int i = 0; i < sb.Length; i++)
+            { 
+                if (sb[i] == '?')
+                {
+                    pos = i;
+                    for (int j = i; j < sb.Length; j++)
+                        {
+                            if (sb[j] == ' ')
+                            {
+                                sb.Replace(' ', '_',j,1);
+                            }
+                        }
                 }
             }
+            Console.WriteLine("My string is: " + sb);
+        }
 
-            /* for (int i = 0; i < myString.Length; i++)
-             {
-                 if (myString[i].Equals("?"))
-                         count ++;
-                 Console.WriteLine(myString[i]);
-             }*/
-            //int index = myString.IndexOf('?');
-            string output = myString.Substring(myString.IndexOf('?') + 1);
+        public static void Task3()
+        {
+            /*Given two arrays of strings a1 and a2 return a sorted array r in lexicographical
+             order of the strings of a1 which are substrings of strings of a2.
+                Example 1:
+            a1 = ["arp", "live", "strong"]
+            a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+            returns ["arp", "live", "strong"]
+            Example 2:
+            a1 = ["tarp", "mice", "bull"]
+            a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+            returns []*/
             
-            Console.WriteLine($"Result is : {count}");
-            Console.WriteLine($"Index is : {index}");
-          //  Console.WriteLine($"Result is : {index}");
-            Console.WriteLine($"Result is : {output}");
+            String [] a1 = {"arp", "live", "strong"};
+            String [] a2 = {"lively", "alive", "harp", "sharp", "armstrong"};
+
+            String[] newArray;
             
+
+
         }
     }
 }
