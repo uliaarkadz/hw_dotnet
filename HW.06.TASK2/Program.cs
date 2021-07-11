23ce94163ccf3dcd5a804c261c1aff02b69c120c
ulia <juliabuiko@gmail.com>
2021-07-11 13:59:39 -0400

    task 2 HW6, created classes for motorcycle and engine

---
+++ HW.06.TASK2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW._06.TASK2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Motorcycle motorcycle = new Motorcycle();
+            motorcycle.GetYear();
+            Console.WriteLine("Enter motorcycle make:");
+            motorcycle.GetMake(Console.ReadLine(); out string make);
+            motorcycle.GetModel("mototpe");
+            motorcycle.GetOdometer(160);
+            motorcycle.GetVinNumber(new Guid());
+            Motorcycle.Engine engine = new Motorcycle.Engine();
+            engine.GetPower(1000);
+            engine.GetVolume(75);
+            engine.GetEngineType("Gasoline");
+        }
+    }
+}
