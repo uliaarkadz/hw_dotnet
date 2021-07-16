65e24478e7bf17f47b4727c1e1a0efe91ae5866f
ulia <juliabuiko@gmail.com>
2021-07-15 23:09:36 -0400

    Task 2 creating house

---
+++ HW.07.TASK02/House.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace HW._07.TASK02
+{
+    public class House
+    {
+        private double _area;
+        private double Area { get; set; }
+
+        public House()
+        {
+            this._area = _area;
+        }
+
+        public House ShowData()
+        {
+            House house = new House();
+            house.ShowData();
+            Console.WriteLine($"I am a house, my area is {_area} m2");
+            return House ShowData();
+        }
+    }
+}
---
+++ HW.07.TASK02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW._07.TASK02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
